74102d41949bb5bd5ceb84eaaec2033bf7df8569
ejohnpaul <johnpaulcemenogu@gmail.com>
2023-10-01 14:45:07 -0400

    first commit

---
+++ sonar-project.js
@@ -0,0 +1,18 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://3.85.17.42:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
+	    'sonar.login': '4003d1d2042a01ea07bcd96ad764b981da592cfe',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
